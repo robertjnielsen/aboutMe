d6605876549fa4a97b51d49be02a261efb52aeef
Robert James Nielsen <robert.j.nielsen@outlook.com>
2019-12-04 19:08:06 -0800

    Updated number and color guessing games.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+/* eslint-disable eol-last */
 'use strict';
+
 alert('Welcome to my About Me page!');
 var firstName = prompt('First, please let me know your first name.');
 var score = 0;
@@ -65,5 +67,49 @@ if (fiveUpper === 'N') {
 } else {
   console.log('Please input Y for Yes, or N for No.');
 }
-// eslint-disable-next-line eol-last
-alert('Thank you for playing my guessing game ' + firstName + '. I hope you enjoyed it! \n You scored ' + score + ' / 5!');
+
+// Random Number Guessing Game.
+
+// Generates a random number between 0 - 10.
+var randomNum = Math.ceil(Math.random() * 10);
+var correctGuess = false;
+
+// Gives the user 4 attempts to guess, while informing them if the # is higher or lower.
+for (var attemptsRemaining = 4; attemptsRemaining > 0; attemptsRemaining--) {
+  var numAnswer = prompt('Guess a number between 1 - 10');
+  if (numAnswer > randomNum) {
+    alert('The correct answer is lower. ' + (attemptsRemaining - 1) + ' attempts remaining.');
+  } else if (numAnswer < randomNum) {
+    alert('The correct number is higher. ' + (attemptsRemaining - 1) + ' attempts remaining.');
+  } else {
+    alert('Correct!');
+    attemptsRemaining = 0;
+    score++;
+  }
+}
+
+// Inform the user of the correct number if all guesses were incorrect.
+if (attemptsRemaining === 0 && correctGuess === false) {
+  alert('The correct answer was ' + randomNum + '.');
+}
+
+// Create an array of correct responses.
+var favColors = ['BLUE', 'GREEN', 'BROWN'];
+
+// Gives the user 6 attempts to guess one of the correct colors.
+for (var colorsRemaining = 6; colorsRemaining > 0; colorsRemaining--) {
+  var colorGuess = prompt('Guess one of my favorite colors.');
+  var colorGuessUp = colorGuess.toUpperCase();
+  if (colorGuessUp === favColors[0] || colorGuessUp === favColors[1] || colorGuessUp === favColors[2]) {
+    alert('Correct! ' + colorGuessUp + ' is one of my favorite colors.');
+    colorsRemaining = 0;
+    score++;
+  } else {
+    alert('That\'s not one of my favorite colors. ' + (colorsRemaining - 1) + ' guesses remaining.');
+  }
+}
+
+// Informs the user of all the correct favorite colors.
+alert('My favorite colors are ' + favColors[0] + ', ' + favColors[1] + ', and ' + favColors[2] + '.');
+
+alert('Thank you for playing my guessing game ' + firstName + '. I hope you enjoyed it! You scored ' + score + ' / 7!');
